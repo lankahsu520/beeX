b1c76b39d13d1ef54f46fba41a715df5fd94f55a
LankaHsu <lankahsu@gmail.com>
2022-12-16 22:37:56 +0800

    update define.mk

--- define.mk
+++ define.mk
@@ -1,6 +1,6 @@
 
 .patched:
-	sed -i "s|#define LIBBEEX_VERSION.*|#define LIBBEEX_VERSION $(LIBBEEX_VERSION)|g" beex_def.h
+	$(PJ_SH_SED) "s|#define LIBBEEX_VERSION.*|#define LIBBEEX_VERSION $(LIBBEEX_VERSION)|g" beex_def.h
 
 	touch $@
 
